e22483cd979de157458902295b3f0d57ee37afc7
zbsedefian <zbsedefian@gmail.com>
2018-03-26 18:06:27 -0400

    order fixed, couple other bugs fixed

--- lsal.c
+++ lsal.c
@@ -14,48 +14,53 @@
 char* formatTime(char* str);
 const char* getFilePermissions(int mode);
 const char* getLastModifiedTime(char *filePath);
+static int cmpstringp(const void *p1, const void *p2);
 
 int main (int argc, char *argv[]) {
     
     DIR *pDir;
+    //struct dirent *pDirent;
     struct dirent **namelist;
     struct stat mystat;
     int i, j, n;
     char buf[512];
-    setlocale(LC_ALL, ""); // ensures alphabetical order
 
-    for (i = argc-1; i > 0; i--) {
+    // Both help ensure alphabetical order
+    setlocale(LC_ALL, ""); 
+    qsort(&argv[1], argc - 1, sizeof(char *), cmpstringp); // sorts argv
+
+    for (i = 1; i < argc; i++) {
         // Point to current directory if none specified
         if (argc == 1) pDir = opendir(".");
         else pDir = opendir(argv[i]);
         
         // For alphabetical sorting
         n = scandir(argv[i], &namelist, NULL, alphasort);
-        
+
         // If directory not found, exit
         if (pDir == NULL) {
-            printf("lsal: cannot access '%s': No such file or directory\n", argv[i]);
-            return 1;
+            printf("lsal: cannot access '%s': No such file or directory", argv[i]);
         }
+        else {
+            // Print directory name
+            if (argc > 2) printf("%s:\n", argv[i]); 
 
-        // Print directory name
-        if (argc > 1) printf("%s:\n", argv[i]); 
-
-        // Print total blocks or whatever that is
-        printf("total %d\n", 4);
+            // Print total blocks or whatever that is
+            printf("total %d\n", 0000);
+        }
 
         // Print contents of directory
         j = 0;
         while (j < n) {
-            sprintf(buf, "%s/%s", argv[i], namelist[i]->d_name);
+            sprintf(buf, "%s", namelist[j]->d_name);
             stat(buf, &mystat);
 
             if (namelist[j]->d_type == DT_DIR || namelist[j]->d_type == DT_REG) {
-                printf("%s %2ld %s %s %4ld %s %s", 
+                printf("%s %2ld %s %s %ld %s %s", 
                     getFilePermissions(mystat.st_mode),
                     mystat.st_nlink,
                     getpwuid(mystat.st_uid)->pw_name,
-                    getgrgid(mystat.st_gid)->gr_name,
+                    getgrgid(mystat.st_gid)->gr_name, 5,
                     mystat.st_size,
                     //formatTime(ctime(&mystat.st_mtime)),
                     getLastModifiedTime(namelist[j]->d_name),
@@ -68,8 +73,8 @@ int main (int argc, char *argv[]) {
             printf("\n");
             j++;
         }
-        
-        if (i > 1) printf("\n");
+            
+        if (i < argc-1) printf("\n");
     }
     free(namelist);
     closedir(pDir);
@@ -81,9 +86,8 @@ int main (int argc, char *argv[]) {
 const char* getLastModifiedTime(char *filePath) {
     struct stat attrib;
     stat(filePath, &attrib);
-    static char date[10];
-    time_t t = time(0);
-    strftime(date, 16, "%b %2d %H:%M ", localtime(&(attrib.st_mtime)));
+    static char date[10] = {0};
+    strftime(date, 15, "%b %2d %H:%M", localtime(&(attrib.st_mtime)));
     return date;
 }
 
@@ -118,9 +122,20 @@ const char* getFilePermissions(int mode) {
 }
 
 
+// https://linux.die.net/man/3/qsort
+static int cmpstringp(const void *p1, const void *p2) {
+    /* The actual arguments to this function are "pointers to
+       pointers to char", but strcmp(3) arguments are "pointers
+       to char", hence the following cast plus dereference */
+
+   return strcmp(* (char * const *) p1, * (char * const *) p2);
+}
+
+
 // // Sort file/dir names
 // static int sortFiles(const struct dirent *ent) {
 //     if (ent->d_name[0] == '.')
 //         return 0;
 //     return 1;
 // }
+
