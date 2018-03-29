422dcb905e17d7aa0852a712f9f11d51a9f471bc
Zachary Sedefian <zbsedefian@gmail.com>
2018-03-29 15:07:39 -0400

    added unix check tp lsal1

--- lsal.c
+++ lsal.c
@@ -102,8 +102,10 @@ int main (int argc, char *argv[])
                 mystat = emptystat;
                 lstat(buf, &mystat);
                 
+                #ifndef OS_Unix
                 if (namelist[j]->d_type == DT_DIR) // print blue if dir
                 {
+                #endif
                     printf("%s %*ld %s %s %*ld %s %c[1;34m%s%c[0m", 
                         getFilePermissions(mystat.st_mode),
                         formatWidthLink,
@@ -117,7 +119,11 @@ int main (int argc, char *argv[])
                         namelist[j]->d_name,
                         27
                     );
+                #ifndef OS_Unix
                 }
+                #endif
+
+                #ifndef OS_Unix
                 if (namelist[j]->d_type == DT_REG) // print normal if file
                 {
                     printf("%s %*ld %s %s %*ld %s %s", 
@@ -134,6 +140,8 @@ int main (int argc, char *argv[])
                 }
 
                 if (namelist[j]->d_type == DT_DIR) printf("/");
+                #endif
+
                 printf("\n");
             }
             if (i < argc-1-dirsNotFound) printf("\n");
